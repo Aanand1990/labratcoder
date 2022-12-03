2dac879873979d1dde68c79a76d944dae65814a4
Aanand Manohar <me@aanandmanohar.com>
2022-12-03 19:15:52 +0530

    building model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Tracker(models.Model):
+    date = models.DateTimeField()
+    weight = models.FloatField(null=True)
+    waist = models.FloatField(null=True)
+    stomach = models.FloatField(null=True)
+    chest = models.FloatField(null=True)
+    butt = models.FloatField(null=True)
+    thighs = models.FloatField(null= True)
+    calves = models.FloatField(null=True)
+    code_written = models.FloatField(null=True)
+
